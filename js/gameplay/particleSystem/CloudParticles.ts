31551548c96da59381c6deb6bee48e58c3ddf77e
Squareys <squareys@googlemail.com>
2023-07-22 11:29:34 +0700

    Tweak cloud particles
    Signed-off-by: Squareys <squareys@googlemail.com>

--- js/gameplay/particleSystem/CloudParticles.ts
+++ js/gameplay/particleSystem/CloudParticles.ts
@@ -2,6 +2,8 @@ import {Component, Object3D, MeshComponent, Material} from "@wonderlandengine/ap
 import {property} from "@wonderlandengine/api/decorators.js";
 import {vec3, quat2} from "gl-matrix";
 
+const tempVec = new Float32Array(3);
+
 /**
 Very simple mesh particles system
 Demostrates spawning of new objects with components
@@ -39,10 +41,10 @@ export class CloudMeshParticles extends Component {
     @property.object()
     spawnObjLocation: Object3D;
 
-    @property.float(0.4)
+    @property.float(0.0)
     gravity: number;
 
-    @property.float(0.04)
+    @property.float(0.35)
     deceleration: number;
 
     public time: number;
@@ -89,7 +91,7 @@ export class CloudMeshParticles extends Component {
             o.getComponent("mesh").material = mat;
         }
     }
-    
+
 
     update(dt: number)
     {
@@ -120,9 +122,7 @@ export class CloudMeshParticles extends Component {
             const vel = this._velocities[i];
             vel[1] -= this.gravity * dt;
 
-            // Apply deceleration based on timeFraction
-            const decelerationFactor = 1 - timeFraction * this.deceleration;
-            vec3.scale(vel, vel, decelerationFactor);
+            vec3.scale(vel, vel, 1 - dt*this.deceleration);
 
             /* Check if particle would collide */
             if (origin[1] + vel[1] * dt <= this.particleScale && vel[1] <= 0) {
@@ -143,28 +143,27 @@ export class CloudMeshParticles extends Component {
             this._objects[i].translateLocal(distance);
 
             this._objects[i].rotateAxisAngleDegObject([0,0,1],this.getRandomNumber(90,1)*dt)  // * (max- min) + min
-			this._objects[i].scaleLocal([this._randomScale, this._randomScale, 1])
-			if(this._fadeFlag){
+            this._objects[i].scaleLocal([this._randomScale, this._randomScale, 1])
+            if(this._fadeFlag) {
                 //@ts-ignore
-				var alpha = this._objects[i].getComponent(MeshComponent).material.color[3];
+                var alpha = this._objects[i].getComponent(MeshComponent).material.color[3];
                 //@ts-ignore
-				if (alpha > 0) this._objects[i].getComponent(MeshComponent).material.color=[1,1,1,alpha*.9999999]
-				//@ts-ignore
-                else this._objects[i].getComponent(MeshComponent).material.color= [1,1,1,0]
-				//console.log(this.objects[i].getComponent(MeshComponent).material.color[3])
-			}
+                if (alpha > 0) this._objects[i].getComponent(MeshComponent).material.color = [1, 1, 1, alpha*.9999999]
+                //@ts-ignore
+                else this._objects[i].getComponent(MeshComponent).material.color = [1, 1, 1, 0]
+            }
         }
     }
 
     getRandomNumber(max: number,min: number){
-		var value = Math.random() * (max - min) + min
-		var orientation = 1;
-		const random = Math.random()
-		if(random === 0 )orientation =  -1
-		else orientation = 1;
-		value *= orientation;
-		return value ;
-	}
+        var value = Math.random() * (max - min) + min
+        var orientation = 1;
+        const random = Math.random()
+        if(random === 0 )orientation =  -1
+        else orientation = 1;
+        value *= orientation;
+        return value ;
+    }
 
     /** Spawn a particle */
     spawn(): void
@@ -178,13 +177,12 @@ export class CloudMeshParticles extends Component {
             //@ts-ignore
             obj.getComponent(MeshComponent).material.color= [1,1,1,1]
             obj.resetTransform();
-            obj.scaleLocal([this.particleScale, this.particleScale, this.particleScale]);
-            obj.rotateAxisAngleDegObject([1,0,0],90)
+            const s = this.particleScale*(Math.random() + 0.5);
+            obj.scaleLocal([s, s, s]);
 
             /* Activate component, otherwise it will not show up! */
             obj.getComponent("mesh").active = true;
 
-            
             if (this.spawnObjLocation != null) {
                 /* Added Spawn on Choosen Object Location instead of self if not null */
                 quat2.getTranslation(this.origin, this.spawnObjLocation.getTransformWorld());
@@ -192,10 +190,12 @@ export class CloudMeshParticles extends Component {
                 quat2.getTranslation(this.origin, this.object.getTransformWorld());
             }
             obj.translateLocal(this.origin);
+            obj.lookAt(this.engine.scene.activeViews[0].object.getPositionWorld(tempVec))
 
             this._velocities[index][0] = Math.random() - 0.5;
-            this._velocities[index][1] = Math.random();
+            this._velocities[index][1] = 0.0;
             this._velocities[index][2] = Math.random() - 0.5;
+
 
             vec3.normalize(this._velocities[index], this._velocities[index]);
             vec3.scale(this._velocities[index], this._velocities[index], this.initialSpeed);
